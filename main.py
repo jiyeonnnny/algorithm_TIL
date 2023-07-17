e3f672da8214294684e3cdf6cca2682b7ccdfe76
dqk86 <dqk8632@gmail.com>
2023-07-18 06:29:02 +0900

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-n,b = map(int, input().split())
+a = dict()
+for i in range(5):
+    a[i] = input().strip()
+
+for i in range(15):
+    for j in range(5):
+        if len(a[j])>i:
+            print(a[j][i],end='')
